8669045c57b498bace974facbcd34a49e477701b
Pardeep Sharma <sharmaji336659@gmail.com>
2024-11-22 17:23:19 +0530

    done

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Profile from "./Profile";
+import "react-image-crop/dist/ReactCrop.css";
+
+
+export default function Home() {
+  return (
+   <div>
+    <Profile/>
+    
+   </div>
+  );
+}
